952d37288baa495a9d6028c5908d1fdc001d4c1f
marqueane <marqueane_64@hotmail.com>
2024-03-19 20:14:23 -0300

    adicão de funções

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,8 +1,33 @@
-function adicionar(produto){
+function alterarQtd(produto, acao){
     const qtd = document.getElementById('qtd_' +  produto)
     const valor = document.getElementById('valor_' +  produto)
     const total = document.getElementById('total_' +  produto)
 
-    qtd.innerHTML++
-    total.innerHTML = qtd.innerHTML * valor.innerHTML
+    if(acao == '-' && qtd.innerHTML == 0){
+        alert('Atenção! Quantidade não permitida.')
+    } else {
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+        const valorTotal = qtd.innerHTML * somenteNumeros(valor.innerHTML)
+        total.innerHTML = formatarValor(valorTotal)
+        soma()
+    }
+}
+
+function soma(){
+    let soma = 0
+
+    for (let i = 1; i < 4; i++){
+        let numero = somenteNumeros(document.getElementById('total_' + i).innerHTML)
+        soma += Number(numero)
+    }
+   
+    document.getElementById('subtotal').innerHTML = formatarValor(soma)
+}
+
+function somenteNumeros(n){
+    return n.replace(/\D/g,'')     
+}
+
+function formatarValor(n){
+    return 'R$ ' + n.toLocaleString('pt-BR')
 }
